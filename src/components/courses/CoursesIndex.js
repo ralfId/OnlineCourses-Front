1ae5d1d250d9f5f1e50a7cf555c199689182aad7
Rafael-ID <49356385+ralfId@users.noreply.github.com>
2023-03-07 17:18:00 -0600

    Update CoursesIndex.js

--- src/components/courses/CoursesIndex.js
+++ src/components/courses/CoursesIndex.js
@@ -24,7 +24,7 @@ export const CoursesIndex = () => {
             <section className="hero container container-pall">
               <div className="hero__image">
                 <img
-                  src="https://a.top4top.io/p_1996ri7a41.png"
+                  src="https://img.freepik.com/foto-gratis/hombre-estudiando-casa-cursos-linea-o-informacion-gratuita-si-mismo-convierte-musico-guitarrista-aislamiento-cuarentena-contra-propagacion-coronavirus-usando-computadora-portatil-telefono-inteligente-auriculares_155003-39514.jpg?w=1380&t=st=1678227591~exp=1678228191~hmac=e1f7d37d677a29ebd56b911dea5cf4106e2965ad8e873ae9830a1aec551933f2"
                   alt="Hero"
                   className="hero__illustration-image"
                   draggable="false"
@@ -33,14 +33,18 @@ export const CoursesIndex = () => {
 
               <div className="hero__text">
                 <h1 className="hero__heading">
-                  Lorem ipsum dolor sit amet consectetur
+                  Online Courses
                 </h1>
+                <h3>Welcome to our online course store! We are excited to offer you a wide variety
+                  of options to help you improve your skills and knowledge in different areas.</h3>
                 <p className="hero__description">
-                  Words Bytes Lists Rich TextHTML Copy Lorem ipsum dolor sit
-                  amet consectetur adipiscing elit imperdiet, sapien per erat
-                  scelerisque lobortis hac habitasse, eu euismod mattis tellus
-                  rutrum porta aliquet. Habitant integer tortor aliquet a
-                  faucibus vehicula massa velit iaculis quam vitae posuere.
+                  On our platform you can find online courses on different topics, from digital marketing to programming,
+                  graphic design, finance, leadership and much more. All our courses are designed to be accessible and practical,
+                  so you can learn effectively and at your own pace.
+                  <br />
+                  <br />
+                  We are sure that you will find in our online course store the training you are looking for to achieve your personal and professional goals.
+                  Thank you for trusting us and start this new learning adventure!
                 </p>
                 <a href="#courses" className="hero__cta hero__cta--primary">
                   Subscribe to a course
@@ -48,15 +52,15 @@ export const CoursesIndex = () => {
               </div>
             </section>
             <Hidden mdDown>
-            <div className="h-divider">
-              <div className="shadow"></div>
-            </div>
+              <div className="h-divider">
+                <div className="shadow"></div>
+              </div>
             </Hidden>
           </Grid>
         </Grid>
 
-        <Paper elevation={3} className="container-py container-mb3">
-          
+        <Paper elevation={3} className="container-py container-mb3" id="courses">
+
           <Grid container spacing={2} className="container-py container-px">
             <Grid item xs={12} md={4}>
               <Card className={useStyles.rootCard} variant="outlined">
@@ -64,7 +68,7 @@ export const CoursesIndex = () => {
                   <CardMedia
                     component="img"
                     alt="Contemplative Reptile"
-                    height="140"
+                    height="170"
                     image="https://images.pexels.com/photos/1181316/pexels-photo-1181316.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
                     title="Contemplative Reptile"
                   />
@@ -195,7 +199,7 @@ export const CoursesIndex = () => {
             </Grid>
           </Grid>
         </Paper>
-      
+
       </Container>
     </>
   );
